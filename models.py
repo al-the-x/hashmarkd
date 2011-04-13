195ad88bb46493f9131612d108000c9e4b844799
David Rogers <david@ethos-development.com>
2011-04-13 17:18:19 -0400

    So I totally didn't need the "Tweet.all_for" or "Tweet.all_from" methods once I had a _real_ "User" object. Refactored. Also, my implementation of "tweets_for" and "tweets_from" needed some refactoring.

--- models.py
+++ models.py
@@ -8,25 +8,30 @@ class User ( db.Model ):
 
 
     @classmethod
+    def get_or_insert_for ( cls, **kwargs ):
+        try: user = tweepy.api.get_user(**kwargs)
+
+        except tweepy.TweepError: return None
+
+        return cls.get_or_insert(user.screen_name,
+            id = user.id_str, screen_name = user.screen_name
+        )
+
+
+    @classmethod
     def for_user_id ( cls, user_id ):
-        if not user_id: return None
-
-        try: twitter_user = tweepy.api.get_user(user_id = user_id)
-
-        except tweepy.error.TweepError: return None
-
-        return cls.get_or_insert(twitter_user.screen_name,
-            id = twitter_user.id_str, screen_name = twitter_user.screen_name
+        return (
+            cls.all().filter('user_id =', user_id).get() or
+            cls.get_or_insert_for(id = user_id)
         )
 
 
     @classmethod
     def for_screen_name ( cls, screen_name ):
-        if not screen_name: return None
-
-        return cls.get_or_insert(screen_name, **dict( (k, str(v)) for k, v in vars(
-            tweepy.api.get_user(screen_name = screen_name)
-        ).items() if k in ( 'id', 'screen_name' ) ))
+        return (
+            cls.all().filter('screen_name =', screen_name).get() or
+            cls.get_or_insert_for(screen_name = screen_name)
+        )
 
 
     def __str__ ( self ): return unicode(self)
@@ -58,18 +63,6 @@ class Tweet ( db.Model ):
         return super(Tweet, cls).all(
             *args, **kwargs
         ).order('-created_at')
-
-
-    @classmethod
-    def all_from ( cls, user_id ):
-        return cls.all().filter('from_user_id =', user_id)
-
-
-    @classmethod
-    def all_to ( cls, user_id ):
-        if not user_id: return cls.all().filter('from_user_id =', None)
-
-        return cls.all().filter('to_user_id =', user_id)
 
 
     def __str__ (self ): return self.__unicode__()
