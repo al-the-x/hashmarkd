572f7d5cfebaf3263ffaf25edc710dea3413ec7f
David Rogers <david@orlandophp.org>
2013-10-26 17:22:57 -0700

    Mostly reformatting to be more Pythonic but also refactoring the logic from the FetchTask into the Tweet model.

--- app/hashmarkd/handlers.py
+++ app/hashmarkd/handlers.py
@@ -4,81 +4,82 @@ from models import Tweet, User
 import decorator, logging, tweepy
 
 @decorator.decorator
-def add_user_to_request ( f, self, screen_name, *args, **kwargs ):
+def add_user_to_request(f, self, screen_name, *args, **kwargs):
     '''
     Pull the "screen_name" parameter and display Tweets for that User,
     if we have any. This is all public data anyway, so we're not concerned
     about authentication yet.
     '''
 
-    self.request.user = User.for_screen_name(
+    self.request.user=User.for_screen_name(
         screen_name or self.request.get('screen_name') or 'hashmarkd'
     )
 
     if not self.request.user:
         ## self.flash('Not a valid user account...')
-        self.request.user = User.for_screen_name('hashmarkd')
+        self.request.user=User.for_screen_name('hashmarkd')
 
     return f(self, screen_name, *args, **kwargs)
 
 
 @decorator.decorator
-def add_pagination ( f, self, screen_name, page, count, *args, **kwargs ):
-    page = page or 1; count = count or 10
+def add_pagination(f, self, screen_name, page, count, *args, **kwargs):
+    page=page or 1; count=count or 10
 
     return f(self, screen_name, page, count, *args, **kwargs)
 
 
-class IndexPage ( RequestHandler ):
+class IndexPage(RequestHandler):
     @add_user_to_request
-    def get ( self, screen_name = None ):
-        user = self.view.user = self.request.user
+    def get(self, screen_name=None):
+        user=self.view.user=self.request.user
 
-        self.view.by_me = user.tweets_from.fetch(limit = 10)
+        self.view.by_me=user.tweets_from.fetch(limit=10)
 
-        self.view.for_me = user.tweets_to.fetch(limit = 10)
+        self.view.for_me=user.tweets_to.fetch(limit=10)
 
         self.render_to_response('index.haml')
 
 
-class TweetsPage ( RequestHandler ):
-    def tweets ( self, user ): pass
+class TweetsPage(RequestHandler):
+    def tweets(self, user): pass
 
 
     @add_user_to_request
     @add_pagination
-    def get ( self, screen_name, page, count ):
-        self.view.user = self.request.user
+    def get(self, screen_name, page, count):
+        self.view.user=self.request.user
 
-        self.view.tweets = self.tweets(self.request.user).fetch(
-            limit = count, offset = count * (page - 1)
+        self.view.tweets=self.tweets(self.request.user).fetch(
+            limit=count, offset=count * (page - 1)
         )
 
         self.render_to_response('tweets.haml')
 
 
-class MarkedByPage ( TweetsPage ):
-    view = dict(name = 'by_me')
+class MarkedByPage(TweetsPage):
+    view=dict(name='by_me')
 
-    def tweets ( self, user ):
+    def tweets(self, user):
         return user.tweets_from
 
 
-class MarkedForPage ( RequestHandler ):
-    name = 'for_me'
+class MarkedForPage(RequestHandler):
+    name='for_me'
 
-    def tweets ( self, user ):
+    def tweets(self, user):
         return user.tweets_to
 
 
-class FetchTask ( RequestHandler ):
-    def get ( self ):
+class FetchTask(RequestHandler):
+    def get(self):
         self.response.out.write('Fetching new tweets: ')
 
         ## Keep GAE from retrying if rate-limited by Twitter...
-        try: results = tweepy.api.search('#markd', filter = 'links')
+        try:
+            results=tweepy.api.search('#markd', filter='links')
 
-        except tweepy.TweepError, error:
+        except tweepy.TweepError as error:
             logging.warning(error)
 
             self.response.out.write(error)
@@ -88,24 +89,18 @@ class FetchTask ( RequestHandler ):
         for result in results:
             try:
                 ## TODO: Refactor into class method of Tweet
-                Tweet.get_or_insert(result.id_str, status_id = result.id_str,
-                    from_user = User.get_or_insert(result.from_user,
-                        id = result.from_user_id_str, screen_name = result.from_user,
-                    ),
-                    to_user = User.for_user_id(result.to_user_id),
-                    created_at = result.created_at, text = result.text
-                )
+                Tweet.from_tweet(result)
 
                 self.response.out.write('.')
 
             ## In case that "User.for_user_id" call fails, ignore this "result" for now...
-            except tweepy.TweepError, error:
+            except tweepy.TweepError as error:
                 logging.warning(error)
 
                 self.response.out.write('X')
 
 
-URLS = [
+URLS=[
     (r'/(\w+)?/?', IndexPage),
     (r'/markd_by/(\w+)/(?:page/(\d+)/)?(?:count/(\d+)/)?', MarkedByPage),
     (r'/markd_for/(\w+)/(?:page/(\d+)/)?(?:count/(\d+)/)?', MarkedForPage),
--- app/hashmarkd/models.py
+++ app/hashmarkd/models.py
@@ -1,14 +1,14 @@
 from google.appengine.ext import db
 import tweepy
 
-class User ( db.Model ):
+class User(db.Model):
     id = db.StringProperty()
 
     screen_name = db.StringProperty()
 
 
     @classmethod
-    def get_or_insert_for ( cls, **kwargs ):
+    def get_or_insert_for(cls, **kwargs):
         try: user = tweepy.api.get_user(**kwargs)
 
         except tweepy.TweepError: return None
@@ -19,7 +19,7 @@ class User ( db.Model ):
 
 
     @classmethod
-    def for_user_id ( cls, user_id ):
+    def for_user_id(cls, user_id):
         return (
             cls.all().filter('user_id =', user_id).get() or
             cls.get_or_insert_for(id = user_id)
@@ -27,20 +27,20 @@ class User ( db.Model ):
 
 
     @classmethod
-    def for_screen_name ( cls, screen_name ):
+    def for_screen_name(cls, screen_name):
         return (
             cls.all().filter('screen_name =', screen_name).get() or
             cls.get_or_insert_for(screen_name = screen_name)
         )
 
 
-    def __str__ ( self ): return unicode(self)
+    def __str__(self): return unicode(self)
 
-    def __unicode__ ( self ):
+    def __unicode__(self):
         return unicode(self.screen_name)
 
 
-class Tweet ( db.Model ):
+class Tweet(db.Model):
     status_id = db.StringProperty()
 
     from_user = db.ReferenceProperty(User,
@@ -59,15 +59,25 @@ class Tweet ( db.Model ):
 
 
     @classmethod
-    def all ( cls, *args, **kwargs ):
+    def all(cls, *args, **kwargs):
         return super(Tweet, cls).all(
             *args, **kwargs
         ).order('-created_at')
 
+    @classmethod
+    def from_tweet(cls, tweet):
+        return cls.get_or_insert(tweet.id_str, status_id=tweet.id_str,
+            from_user=User.get_or_insert(tweet.from_user,
+                id=tweet.from_user_id_str, screen_name=tweet.from_user,
+            ),
+            to_user=User.for_user_id(tweet.to_user_id),
+            created_at=tweet.created_at, text=tweet.text
+        )
 
-    def __str__ (self ): return self.__unicode__()
+    def __str__ (self):
+        return self.__unicode__()
 
-    def __unicode__ ( self ):
+    def __unicode__(self):
         return unicode(self.text)
 
 
